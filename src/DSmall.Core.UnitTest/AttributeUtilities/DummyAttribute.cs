9bf90861db282a2981d34cf0f8eabf44fe072971
Davesmall28 <david.small@dsmall.co.uk>
2015-03-01 08:15:50 +0000

    Update DSmall.Core to use NUnit unit test framework.

---
+++ src/DSmall.Core.UnitTest/AttributeUtilities/DummyAttribute.cs
@@ -0,0 +1,20 @@
+﻿namespace DSmall.Core.UnitTest
+{
+    using System;
+
+    /// <summary>The dummy attribute.</summary>
+    [Serializable]
+    [AttributeUsage(AttributeTargets.All)]
+    public class DummyAttribute : Attribute
+    {
+        /// <summary>Initialises a new instance of the <see cref="DummyAttribute"/> class.</summary>
+        /// <param name="dummyValue">The dummy value.</param>
+        public DummyAttribute(int dummyValue)
+        {
+            DummyValue = dummyValue;
+        }
+
+        /// <summary>Gets or sets the dummy value.</summary>
+        public int DummyValue { get; set; }
+    }
+}
---
+++ src/DSmall.Core.UnitTest/AttributeUtilities/WhenRetrievingAnExistingAttributeHeldAgainstAField.cs
@@ -0,0 +1,44 @@
+﻿namespace DSmall.Core.UnitTest
+{
+    using DSmall.UnitTest.Core;
+    using NUnit.Framework;
+
+    /// <summary>The when retrieving an existing attribute held against a field.</summary>
+    [TestFixture]
+    public class WhenRetrievingAnExistingAttributeHeldAgainstAField : SpecificationBase
+    {
+        private CustomAttributeUtilitiesSpecificationsFixture testFixture;
+
+        private DummyAttribute result;
+
+        /// <summary>The expected attribute should be returned.</summary>
+        [Test]
+        public void ExpectedAttributeShouldBeReturned()
+        {
+            Assert.IsInstanceOf(typeof(DummyAttribute), result);
+        }
+
+        /// <summary>The expected attribute should hold correct values.</summary>
+        [Test]
+        public void ExpectedAttributeShouldHoldCorrectValues()
+        {
+            Assert.AreEqual(10, result.DummyValue);
+        }
+
+        /// <summary>The because of.</summary>
+        protected override void BecauseOf()
+        {
+            base.BecauseOf();
+            result = testFixture.UnderTest.GetAttributeFor<DummyAttribute, DummyClass>(dummy => dummy.DummyStringField);
+        }
+
+        /// <summary>The context.</summary>
+        protected override void Context()
+        {
+            base.Context();
+
+            testFixture = new CustomAttributeUtilitiesSpecificationsFixture();
+            testFixture.PerformTestSetup();
+        }
+    }
+}
