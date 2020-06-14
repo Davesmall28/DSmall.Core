551bd63cb775729b726860b5515be7b071e2204c
davesmall28 <dave@dsmall.co.uk>
2020-06-14 23:01:04 +0100

    Update solution to .Net v4.6.2 and slimline build process.

--- Springboard365.Core/NuGet/NuGetPush.bat
+++ Springboard365.Core/NuGet/NuGetPush.bat
@@ -1,7 +1,7 @@
-SET packageVersion=1.1.0
+SET packageVersion=2.0.0
 
-NuGet.exe pack ../Springboard365.Core.csproj -Build -symbols -Version %packageVersion%
+NuGet.exe pack ../Springboard365.Core.csproj -Build -Symbols -Version %packageVersion% -Properties Configuration=Release
 
-NuGet.exe push Springboard365.Core.%packageVersion%.nupkg
+NuGet.exe push Springboard365.Core.%packageVersion%.nupkg -source "https://api.nuget.org/v3/index.json"
 
 pause
